0a84101df5fe05329fedf36561ba35dd8cb271b6
devezhang@gmail.com <devezhang@gmail.com>
2018-08-31 00:23:54 +0800

    个人中心基本信息完善OK

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, g, redirect
+from flask import render_template, g, redirect, request, jsonify
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 
 
 @profile_blu.route("/info")
@@ -20,8 +21,29 @@ def user_info():
     return render_template("news/user.html", data=data)
 
 
-
-
-
-
+@profile_blu.route("/base_info", methods=["GET", "POST"])
+@user_login_data
+def base_info():
+    """基本资料"""
+    # 不同的请求函数做不同的事情
+    if request.method == "GET":
+        return render_template("news/user_base_info.html", data={"user": g.user})
+    else:
+        # 代表修改数据
+        # 1.取参
+        params = request.json
+        nick_name = params.get("nick_name")
+        signature = params.get("signature")
+        gender = params.get("gender")
+        # 2.校参
+        if not all([nick_name, signature, gender]):
+            return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+        if gender not in ("WOMAN", "MAN"):
+            return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+        # 3.修改数据
+        user = g.user  # 当前用户,直接赋值即是修改，会自动提交
+        user.signature = signature
+        user.nick_name = nick_name
+        user.gender = gender
+        return jsonify(errno=RET.OK, errmsg="OK")
 
