b1040589bc1d872a0bfd81431fda1c33d317fa61
devezhang@gmail.com <devezhang@gmail.com>
2018-09-06 12:41:31 +0800

    修复个人发布新闻内容为空bug

--- info/static/news/js/user_news_release.js
+++ info/static/news/js/user_news_release.js
@@ -13,6 +13,18 @@ $(function () {
         // ajaxSubmit使用ajax的方式去模拟点击提交按钮的操作
         // ajaxSubmit自动收集带name的input标签，将name和value拼接之后作为ajax提交的数据提交到后台
         $(this).ajaxSubmit({
+
+            beforeSubmit: function (request) {
+                // 在提交之前，对参数进行处理
+                for(var i=0; i<request.length; i++) {
+                    var item = request[i]
+                    if (item["name"] == "content") {
+                        // 前段框架，将value值渲染成富文本返回
+                        item["value"] = tinyMCE.activeEditor.getContent()
+                    }
+                }
+            },
+
             url: "/user/news_release",
             type: "POST",
             headers: {
